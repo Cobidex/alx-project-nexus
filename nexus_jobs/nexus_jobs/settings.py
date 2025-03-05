83e4863bcfac38beb23c31acbad95d2c46465e68
Stephen <stevobialor@gmail.com~>
2025-03-05 19:18:45 +0100

    format error messages

--- nexus_jobs/nexus_jobs/settings.py
+++ nexus_jobs/nexus_jobs/settings.py
@@ -161,7 +161,6 @@ LOGGING = {
     },
 }
 
-
 WSGI_APPLICATION = 'nexus_jobs.wsgi.application'
 
 MEDIA_URL = '/media/'
@@ -225,3 +224,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "team@medukul.com")
+EMAIL_USE_SSL = False
